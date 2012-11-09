de50911ab4d76c244b00b827e874992fae13fdcc
Mike Karolow <mike@yesware.com>
2012-11-09 15:37:34 -0500

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  'mike@yesware.com'
 license           'WTFPL'
 description       'Installs Ruby 1.9 from source'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.mdown'))
-version           '1.1'
+version           '1.1.0'
 supports          'ubuntu'
 
 recipe            'ruby19', 'Installs Ruby 1.9 from source.'
